e707d797a664bc5055e3ba9d9a86f88eadddf293
Calvin McElroy <fourwallsstudio@gmail.com>
2017-07-23 14:45:43 -0400

    setup redux

---
+++ frontend/components/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Root = () => {
+  return (
+    <div>soop world</div>
+  )
+}
+
+export default Root;
---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,10 @@
+
+const defaultState = {
+
+}
+
+const userReducer = (state = defaultState, action) => {
+  return state
+}
+
+export default userReducer
